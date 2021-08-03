8781bda3cb80f00e2a2f4ab13225167951b03486
Catherine Cheatle <48526094+clcheatle@users.noreply.github.com>
2021-08-03 12:39:09 -0400

    testing path

--- Shopping/Shopping.API/Data/ProductContext.cs
+++ Shopping/Shopping.API/Data/ProductContext.cs
@@ -37,7 +37,7 @@ namespace Shopping.API.Data
                 new Product()
                 {
                     Name = "IPhone X",
-                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    Description = "This cellphone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
                     ImageFile = "product-1.png",
                     Price = 950.00M,
                     Category = "Smart Phone"
